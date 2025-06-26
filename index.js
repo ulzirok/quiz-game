843bd61744f4c5e66cc86e53f025fe8200c99d99
ulzirok <ulzirok@gmail.com>
2025-06-26 16:09:33 +0500

    feat: add quiz logic and a function that shows the result

--- index.js
+++ index.js
@@ -9,29 +9,59 @@ const questions = [
 		correct: 4,
 	},
 	{
-		question: "Что означает CSS?",
+		question: "Какой результат у выражения undefined == null?",
 		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-		correct: 2,
-	},
-	{
-		question: "Что означает HTML?",
-		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
+			"true",
+			"false",
+			"undefined",
+			"Ошибка",
 		],
 		correct: 1,
+	},
+	{
+		question: "Что делает setTimeout(function, 1000)?",
+		answers: [
+			"Запускает функцию немедленно",
+			"Останавливает выполнение скрипта",
+			"Выполняет функцию через 1 секунду",
+			"Повторяет функцию каждые 1 секунду",
+		],
+		correct: 3,
 	},
 	{
 		question: "В каком году был создан JavaScript?",
 		answers: ["1996", "1995", "1994", "все ответы неверные"],
 		correct: 2,
+	},
+	{
+		question: "Какое ключевое слово используется для объявления переменной в JavaScript?",
+		answers: ["let", "var", "const", "все перечисленные"],
+		correct: 4,
+	},
+	{
+		question: "Какой тип данных возвращает typeof null?",
+		answers: ["null", "object", "undefined", "boolean"],
+		correct: 2,
+	},
+	{
+		question: "Какой метод используется для добавления элемента в конец массива?",
+		answers: [".shift()", ".unshift()", ".push()", ".pop()"],
+		correct: 3,
+	},
+	{
+		question: "Что делает оператор === в JavaScript?",
+		answers: ["Сравнивает только значения", "Присваивает значение", "Сравнивает значения и типы ", "Преобразует в строку"],
+		correct: 3,
+	},
+	{
+		question: "Какой будет результат Boolean(0)?",
+		answers: ["true", "false", "null", "undefined"],
+		correct: 2,
+	},
+	{
+		question: "Что делает метод querySelector()?",
+		answers: ["Удаляет элемент со страницы", "Добавляет элемент в DOM", "Находит элемент по ID", "Находит первый элемент по CSS-селектору"],
+		correct: 4,
 	},
 ];
 let score = 0
@@ -65,11 +95,62 @@ showQuestion()
 
 function checkAnswer () {
 	const checkedRadio = listContainer.querySelector('input[type="radio"]:checked')
+	
 	if (!checkedRadio) {
 		submitBtn.blur()
 		return
 	}
 	
+	const userAnswer = parseInt(checkedRadio.value)
+	if (userAnswer === questions[questionIndex]['correct']) {
+		score++
+	}
+	
+	if (questionIndex !== questions.length - 1) {
+		questionIndex++
+		clearPage()
+		showQuestion()
+		return
+	}
+	clearPage()
+	showResults()
+	
+}
+
+function showResults () {
+	const resultsTemplate = `
+	  <h2 class="title">%title%</h2>
+	  <h3 class="summary">%message%</h3>
+	  <p class="result">%result%</p>
+	 `
+	let title, message
+	
+	if (score === questions.length) {
+		title = 'Поздравляю! 🎉'
+		message = 'Вы ответили верно на все вопросы! 😎👍'
+		
+	}
+	else if ((score * 100) / questions.length >= 50) {
+		title = 'Неплохой результат! 😉'
+		message = 'Вы дали более половины правильных ответов! 👍'
+	}
+	else {
+		title = 'Стоит постараться 😟💪'
+		message = 'Пока у Вас меньше половины правильных ответов 🥺'
+	}
+	
+	let result = `${score} ✅ из ${questions.length}`
+	
+	const finalMessage = resultsTemplate
+	.replace('%title%', title)
+	.replace('%message%', message)
+	.replace('%result%', result)
+	
+	headerContainer.innerHTML = finalMessage
+	
+	submitBtn.blur()
+	submitBtn.innerHTML = 'Начать заново'
+	submitBtn.addEventListener('click', () => history.go())
 }
 
 submitBtn.addEventListener('click', checkAnswer)
